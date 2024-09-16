3cb4b6ee3af3a19295cb394d265966efb25cf975
shermuhammad786 <sherabro141@gmail.com>
2024-09-16 12:48:40 +0500

    hellow world from express

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("Hello World! from express")
     console.log("object");
 })
 
